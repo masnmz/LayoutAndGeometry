018cd855a462058e2e50045e57ed5e286130a11d
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-09-18 14:00:42 +0100

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Mehmet Alp Sönmez on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
