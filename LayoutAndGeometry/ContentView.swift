11b588c50ac6c81ea868ff51fd64734630d91e49
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-09-19 18:43:59 +0100

    ScrollView effects using visualEffect() and scrollTargetBehavior()

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,28 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
                 ForEach(1..<20) { number in
-                    GeometryReader { proxy in
                         Text("Number \(number)")
                             .font(.largeTitle)
                             .padding()
                             .background(.red)
-                            .rotation3DEffect(
-                                .degrees(-proxy.frame(in: .global).minX / 8),
-                                axis: (x: 0.0, y: 1.0, z: 0.0)
-                            )
-                            .frame(width: 200, height: 200)
-                    }
-                    .frame(width: 200, height: 200)
+                            .visualEffect { content, proxy in
+                                content
+                                    .rotation3DEffect(
+                                        .degrees(-proxy.frame(in: .global).minX / 8),
+                                        axis: (x: 0.0, y: 1.0, z: 0.0)
+                                    )
+                              
+                            }
+                          
                 }
             }
+            .scrollTargetLayout()
         }
+        .scrollTargetBehavior(.viewAligned)
+
     }
+        
 }
 
 #Preview {
