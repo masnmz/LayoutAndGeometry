4a8c2bac893d0a8f6274ffb7ecab979310bbdc5f
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-09-19 18:04:06 +0100

    Understanding frames and coordinates inside GeometryReader

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,19 +7,45 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct OuterView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(0..<10) { position in
-                Text("Number \(position)")
-                    .alignmentGuide(.leading) { _ in
-                        Double(position) * -10
+        VStack {
+            Text("Top")
+            
+            InnerView()
+                .background(.green)
+            
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("LEft")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY) y")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY) y")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY) y")
+
+
                     }
             }
+            .background(.orange)
+            Text("Right")
         }
-        .background(.red)
-        .frame(width: 400, height: 400)
-        .background(.blue)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
