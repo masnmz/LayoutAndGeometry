a2343404d83dbc9e527feb06e037259219dabcde
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-09-23 14:50:56 +0100

    Hacking With Swift Day 94 challenges

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,32 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { number in
-                        Text("Number \(number)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .visualEffect { content, proxy in
-                                content
-                                    .rotation3DEffect(
-                                        .degrees(-proxy.frame(in: .global).minX / 8),
-                                        axis: (x: 0.0, y: 1.0, z: 0.0)
-                                    )
-                              
-                            }
-                          
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(hue: min(proxy.frame(in: .global).minY / fullView.frame(in: .global).maxY, 1), saturation: 1, brightness: 1))
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(proxy.frame(in: .global).minY < 200 ? 0.1 : 1.0)
+                            .scaleEffect(max(0.5, min(1.5, 1 + (proxy.frame(in: .global).minY - fullView.size.height / 2) / fullView.size.height)))
+                    }
+                    .frame(height: 40)
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
-
     }
-        
 }
 
 #Preview {
